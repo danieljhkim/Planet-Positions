bb65c7a345aecb5df0df2c748a97e3ad455ec98d
danieljhkim <danieljhkim1@gmail.com>
2022-07-30 14:14:45 -0400

    code clean up and resize

---
+++ src/components/canvas/constants.js
@@ -0,0 +1,67 @@
+const EARTH = {
+  radius: 140,
+  year: 365*24*60*60*1000,
+  color: '#009432',
+  size: 197/13,
+  position: .48
+};
+
+const MERCURY = {
+  radius: 50,
+  year: 225*24*60*60*1000,
+  color: 'rgb(100,80,80)',
+  size: 50/6,
+  position: .04
+};
+
+const VENUS = {
+  radius: 90,
+  year: 365*24*60*60*1000,
+  color: 'brown',
+  size: 177/13,
+  position: .64
+};
+
+const MARS = {
+  radius: 200,
+  year: 687*24*60*60*1000,
+  color: '#EA2027',
+  size: 56/5,
+  position: .26
+};
+
+const JUPYTER = {
+  radius: 240,
+  year: 11.862*365*24*60*60*1000,
+  color: 'rgb(255,140,0)',
+  size: 22,
+  position: .8
+};
+
+const SATURN = {
+  radius: 300,
+  year: 29.457*365*24*60*60*1000,
+  color: 'rgb(255,222,173)',
+  size: 15,
+  position: .86
+};
+
+const URANUS = {
+  radius: 330,
+  year: 84.011*365*24*60*60*1000,
+  color: 'rgb(176,196,222)',
+  size: 15,
+  position: .21
+};
+
+const NEPTUNE = {
+  radius: 400,
+  year: 164.79*365*24*60*60*1000,
+  color: 'rgb(0,0,200)',
+  size: 15,
+  position: .09
+};
+
+const TUNIX = 1549499771000;
+
+module.exports = { TUNIX, EARTH, MERCURY, VENUS, MARS, JUPYTER, SATURN, URANUS, NEPTUNE };
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,10 +1,25 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { TUNIX, EARTH, MERCURY, VENUS, MARS, JUPYTER, SATURN, URANUS, NEPTUNE } from './constants'
 import './styles.css'
 
 function Canvas(props) {
   const canvasRef = useRef(null);
   const [dateTime, setDateTime] = useState(Date.now());
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+
+  const calculateSize = () => {
+    let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if(width < 600) {
+      width = width - 50;
+    } else if (width >= 600 && width < 800) {
+      width = width - 200;
+    } else {
+      width = width / 2;
+    }
+    width = Math.min(width, 600)
+    return width;
+  }
+  
+  const [screenWidth, setScreenWidth] = useState(calculateSize());
   
   useEffect(() => {
     const canvas = canvasRef.current
@@ -19,46 +34,12 @@ function Canvas(props) {
     drawPlanets(sratio, context);
   }, [dateTime, screenWidth]);
 
-  const calculateSize = () => {
-    let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if(width < 600) {
-      width = width - 50;
-    } else if (width >= 600 && width < 800) {
-      width = width - 200;
-    } else {
-      width = width / 2;
-    }
-    setScreenWidth(Math.min(width, 600));
-  }
 
   window.addEventListener("resize", ()=> {
-    calculateSize();
-  });
-  
-  const planets = (Q, r, year, color, size, sratio, context) => {        
-    const tunix = 1549499771000;
-    let t = ((dateTime - tunix) / year) + Q;
-    if (t >= 0){
-      t = t - Math.floor(t);
-    } else {
-      t = Math.abs(t);
-      t = Math.ceil(t) - t;
-    }
+    const width = calculateSize();
+    setScreenWidth(width);
 
-    size = size * sratio;
-    let rr = r * sratio;
-    let x = 400 * sratio + Math.cos(t*2*Math.PI) * rr;
-    let y = 400 * sratio - Math.sin(t*2*Math.PI) * rr;                                        
-    x = Math.round(x);
-    y = Math.round(y);
-    context.shadowBlur = '30';
-    context.shadowColor = 'rgb(0,0,0)';
-    context.fillStyle = color;
-    context.beginPath();
-    context.arc(x, y, size, 0, Math.PI*2, false);
-    context.closePath();       
-    context.fill();
-  }
+  });
 
   const drawSun = (width, height, sratio, context) => {
     const centreX = width/2;
@@ -80,64 +61,56 @@ function Canvas(props) {
   }
 
   const drawPlanets = (sratio, context) => {
-    const mercyear = 88*24*60*60*1000;
-    const vyear = 225*24*60*60*1000;       
-    const eyear = 365*24*60*60*1000;
-    const marsyear = 687*24*60*60*1000;
-    const jyear = 11.862*365*24*60*60*1000;
-    const syear = 29.457*365*24*60*60*1000;
-    const uyear = 84.011*365*24*60*60*1000;
-    const nyear = 164.79*365*24*60*60*1000;
 
-    const merccolor = 'rgb(100,80,80)';
-    const vcolor = 'brown';
-    const ecolor = '#009432';
-    const marscolor = '#EA2027';
-    const jcolor = 'rgb(255,140,0)';
-    const scolor = 'rgb(255,222,173)';
-    const ucolor = 'rgb(176,196,222)';
-    const ncolor = 'rgb(0,0,200)';
+    const planets = (planetOBj, sratio, context) => {        
+      let t = ((dateTime - TUNIX) / planetOBj.year) + planetOBj.position;
+      if (t >= 0){
+        t = t - Math.floor(t);
+      } else {
+        t = Math.abs(t);
+        t = Math.ceil(t) - t;
+      }
+      const size = planetOBj.size * sratio;
+      const normalizedRatio = planetOBj.radius * sratio;
+      const x = Math.round(400 * sratio + Math.cos(t * 2 * Math.PI) * normalizedRatio);
+      const y = Math.round(400 * sratio - Math.sin(t * 2 * Math.PI) * normalizedRatio);
+      context.shadowBlur = '30';
+      context.shadowColor = 'rgb(0,0,0)';
+      context.fillStyle = planetOBj.color;
+      context.beginPath();
+      context.arc(x, y, size, 0, Math.PI*2, false);
+      context.closePath();       
+      context.fill();
+    }
 
-    const mercsize = 50/6;
-    const vsize = 177/13;
-    const esize = 197/13;
-    const marssize = 56/5;
-    const jsize = 22;
-    const ssize = 15;
-    const usize = 15;
-    const nsize = 15;
-
-    var mercrad=50; var vrad=90; var erad=140; var marsrad=200; var jrad=240; var srad=300; var urad=330; let nrad=400;      
-    let x; let y;
-
-    const mercury = planets(.04, mercrad, mercyear, merccolor, mercsize, sratio, context);
-    const venus = planets(.64, vrad, vyear, vcolor, vsize, sratio, context);
-    const earth = planets(.48, erad, eyear, ecolor, esize, sratio, context);
-    const mars = planets(.26, marsrad, marsyear, marscolor, marssize, sratio, context);
-    const jupiter = planets(.8, jrad, jyear, jcolor, jsize, sratio, context);
-    const saturn = planets(.86, srad, syear, scolor, ssize, sratio, context);
-    const uranus = planets(.21, urad, uyear, ucolor, usize, sratio, context);
-    const neptune= planets(.09, nrad, nyear, ncolor, nsize, sratio, context);
+    planets(MERCURY, sratio, context);
+    planets(VENUS, sratio, context);
+    planets(EARTH, sratio, context);
+    planets(MARS, sratio, context);
+    planets(JUPYTER, sratio, context);
+    planets(SATURN, sratio, context);
+    planets(URANUS, sratio, context);
+    planets(NEPTUNE, sratio, context);
   }
 
   const drawWritings = (sratio, context) => {        
     context.fillStyle = 'rgb(255,255,255)';
-    var title = "Solar Clock";
+    const title = "Solar Clock";
     var fontsize = 50 * sratio;
     var fonttext = "bold " + fontsize + "px " + "serif";
     context.font = fonttext;
     context.fillText(title, 11*sratio, 50*sratio);
-    var descript = 'Planetary Tracking System';
+    const descript = 'Planetary Tracking System';
     var fontsize = 23 * sratio;
     var fonttext = fontsize + "px " + "serif";          
     context.font = fonttext;
     context.fillText(descript, 12*sratio, 80*sratio);
-    var currentdate = new Date(dateTime);
+    let currentdate = new Date(dateTime);
     var fontsize = 18 * sratio;
     var fonttext = fontsize + "px " + "serif";
     context.font = fonttext;
     context.fillText(currentdate, 13*sratio, 110*sratio);
-    var name = 'Operator: DJHK';
+    const name = 'Operator: DJHK';
     var fontsize = 20 * sratio;
     var fonttext = fontsize + "px " + "serif";
     context.font = fonttext;
